38a4f2edacc894c25ca71eb3b31681dd8e06e61f
Engström <joel.engstrom@if.se>
2023-04-06 11:01:52 +0200

    adding minimize

--- public/js/fetchBotResponse.js
+++ public/js/fetchBotResponse.js
@@ -1,14 +1,21 @@
 export async function fetchBotResponse(userInput) {
-  const response = await fetch(
-    "https://combis.netlify.app/.netlify/functions/getBotResponse",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ userInput }),
-    }
-  );
+  let url;
+
+  if (window.location.host === "combis.netlify.app") {
+    url = "https://combis.netlify.app/.netlify/functions/getBotResponse";
+  } else if (window.location.host === "combis.se") {
+    url = "https://combis.se/.netlify/functions/getBotResponse";
+  } else {
+    throw new Error("Unsupported domain");
+  }
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ userInput }),
+  });
 
   if (!response.ok) {
     throw new Error(`Error: ${response.statusText}`);
